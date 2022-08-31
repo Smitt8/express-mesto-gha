0af98fb067c63525a2362b5811b2a66e2765f029
Igor Prokazov <smitt8@yandex.ru>
2022-08-31 21:25:27 +0300

    refac: Упростить проверку ошибки

--- utils/utils.js
+++ utils/utils.js
@@ -1,9 +1,7 @@
 const { ERR_BAD_INPUT, ERR_SERVER_ERR } = require('./consts');
 
 const checkErr = (err, res) => {
-  if ((err.kind === 'ObjectId') || (err.errors.name && err.errors.name.name === 'ValidatorError')
-  || (err.errors.about && err.errors.about.name === 'ValidatorError')
-  || (err.errors.avatar && err.errors.avatar.name === 'ValidatorError')) {
+  if ((err.kind === 'ObjectId') || (err.name === 'ValidationError')) {
     return res.status(ERR_BAD_INPUT).send({ message: 'Некорректный запрос' });
   }
   return res.status(ERR_SERVER_ERR).send({ message: 'Ошибка сервера' });
